f982d6c72cab7f721d97d5164a975ec5c8c5eba4
duygusen <seenduygu@gmail.com>
2023-04-15 19:45:18 +0300

    signup eklendi

---
+++ filmblogu/Controllers/SignupController.cs
@@ -0,0 +1,28 @@
+﻿using BlogAppADO.DataAccess;
+using filmblogu.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace filmblog.Controllers
+{
+    public class SignupController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginModel model)
+        {
+            UserDal userDal = new UserDal();
+            LoginUser user = userDal.Login(model);
+
+
+
+            HttpContext.Session.SetString("Signup", user.RoleId.ToString());
+
+            return Content("Üyelik Oluşturulmuştur.");
+        }
+    }
+}
