4b6d3cae5f5ba751d962fe8a51d7c1ea26fbeb72
duygusen <seenduygu@gmail.com>
2023-04-16 02:37:32 +0300

    adminpaneline başlandı

---
+++ filmblogu/Controllers/Admin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace filmblogu.Controllers;
+
+public class Admin
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Panel()
+    {
+        return View();
+    }
+
+    public IActionResult Guncelle()
+    {
+        return View();
+    }
+}
+
